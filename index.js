43958ab17b0d220a1b5d3c71d32de7e37d5ac0f7
kadowakiup <kadowaki.universal.prime@gmail.com>
2026-03-29 13:23:04 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -20,38 +20,67 @@
 //     alert("送信しました");
 // }
 
-const LIFF_ID = "2009569390-ToBfmkCN";
 
-async function main() {
-    await liff.init({ liffId: LIFF_ID });
+// const LIFF_ID = "2009569390-ToBfmkCN";
+// async function main() {
+//     await liff.init({ liffId: LIFF_ID });
 
-    if (!liff.isLoggedIn()) {
-        liff.login();
-        return;
+//     if (!liff.isLoggedIn()) {
+//         liff.login();
+//         return;
+//     }
+// }
+// main();
+
+// async function send() {
+//     const date = document.getElementById("date").value;
+//     console.log("inClient:", liff.isInClient());
+//     if (!liff.isInClient()) {
+//         alert("LINEアプリ内で開いてください");
+//         return;
+//     }
+//     await liff.sendMessages([
+//         {
+//             type: "text",
+//             text: "シフト変更＞" + date
+//         }
+//     ]);
+//     alert("送信しました");
+//     liff.closeWindow();
+// }
+
+
+window.onload = function () {
+  liff.init({
+    liffId: "2009569390-ToBfmkCN"
+  })
+  .then(() => {
+    console.log("LIFF初期化成功");
+  })
+  .catch((err) => {
+    console.error("LIFF初期化エラー:", err);
+  });
+};
+
+function send() {
+  // LIFFが開かれているかチェック
+  if (!liff.isInClient()) {
+    alert("LINEアプリ内で開いてください");
+    return;
+  }
+
+  liff.sendMessages([
+    {
+      type: "text",
+      text: "テスト"
     }
-}
-
-main();
-
-async function send() {
-
-    const date = document.getElementById("date").value;
-
-    console.log("inClient:", liff.isInClient());
-
-    if (!liff.isInClient()) {
-        alert("LINEアプリ内で開いてください");
-        return;
-    }
-
-    await liff.sendMessages([
-        {
-            type: "text",
-            text: "シフト変更＞" + date
-        }
-    ]);
-
+  ])
+  .then(() => {
     alert("送信しました");
-
-    liff.closeWindow();
+    liff.closeWindow(); // 送信後に画面閉じる
+  })
+  .catch((err) => {
+    console.error("送信エラー:", err);
+    alert("送信に失敗しました");
+  });
 }
